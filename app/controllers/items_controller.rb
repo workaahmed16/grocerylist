debb23d15b7069105586eeaa0763f16cd267d1c3
Ahmed Ahmed <critenks29@gmail.com>
2016-03-27 06:13:15 +0000

    Jingle Bells

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,16 @@ class ItemsController < ApplicationController
 
 	def new
 		@item = Item.new
-
-
 	end
+
+	def create
+    	Item.create(item_params)
+    	redirect_to root_path
+  	end
+
+  	private
+
+  	def item_params
+    	params.require(:item).permit(:name, :quantity)
+  	end
 end
